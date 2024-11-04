d31ec5d0b8fcf5b03e3826cf18212772011fce3b
amauri <alcemiro@hotmail.com>
2024-11-04 18:40:18 -0300

    commit inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { QuizzComponent } from './components/quizz/quizz.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent, QuizzComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'projeto-buzzfeed';
+}
